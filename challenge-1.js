1afd02b962d2e5f91878414b920ad2bf3f16e404
Madina <madinatussupova0820@gmail.com>
2022-05-20 15:22:40 +0600

    ternary operator

--- challenge-1.js
+++ challenge-1.js
@@ -32,13 +32,13 @@ if(markBmi > johnBmi) {
 
 // CHALLENGE 3
 
-const averageDoplhins = Math.round((96 + 108 + 89)/3);
-const averageKoalas = Math.round((88 + 91 + 110)/3);
+// const averageDoplhins = Math.round((96 + 108 + 89)/3);
+// const averageKoalas = Math.round((88 + 91 + 110)/3);
 
 // const averageDoplhins = Math.round((97 + 112 + 101)/3);
 // const averageKoalas = Math.round((109 + 95 + 106)/3);
 
-
+/*
 if(averageDoplhins > averageKoalas) {
 	console.log(`Doplhins are winners!`);
 } else if (averageKoalas > averageKoalas) {
@@ -63,3 +63,18 @@ if(averageDoplhins >= 100 && averageDoplhins > averageKoalas){
 } else if(averageKoalas === averageDoplhins && averageDoplhins >= 100 && averageKoalas >= 100) {
 	console.log(`That's a draw!`);
 } //THAT'S A DRAW
+*/
+
+// CHALLENGE 4
+
+const bill = 275;
+const bill2 = 40;
+const bill3 = 430;
+
+const tip = 50 <= bill && bill <= 250 ? bill * 0.15 : bill * 0.2;
+const tip2 = 50 <= bill2 && bill2 <= 250 ? bill * 0.15 : bill2 * 0.2;
+const tip3 = 50 <= bill3 && bill3 <= 250 ? bill * 0.15 : bill3 * 0.2;
+
+const value = console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`)
+const value2 = console.log(`The bill was ${bill2}, the tip was ${tip2}, and the total value ${bill2 + tip2}`)
+const value3 = console.log(`The bill was ${bill3}, the tip was ${tip3}, and the total value ${bill3 + tip3}`)
