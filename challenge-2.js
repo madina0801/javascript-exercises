24a8bcc7961ba6581b83cb70e655e94564eac15b
Madina <madinatussupova0820@gmail.com>
2022-05-26 23:44:21 +0600

    arrays

--- challenge-2.js
+++ challenge-2.js
@@ -1,4 +1,5 @@
 // CHALLENGE 1
+/*
 const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
 const scoreDolphins = calcAverage(85, 54, 41);
@@ -15,3 +16,22 @@ const checkWinner = function(dolphinsAvg, koalasAvg) {
 }
 
 checkWinner(scoreDolphins, scoreKoalas);
+*/
+//CHALLENGE 2
+function calcTip(bill) {
+	if(50 <= bill && bill <= 300) {
+		return bill * 0.15;
+	} else {
+		return bill * 0.20;
+	}
+};
+
+bills = [125, 555, 44];
+
+const tips = bills.map(bill => {
+	return calcTip(bill);
+})
+
+const total = bills.map((num, idx) => {
+	return num + tips[idx];
+});
