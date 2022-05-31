5caf9c4a09f096a92e230fe9bba8e113c0e456a9
Madina <madinatussupova0820@gmail.com>
2022-05-31 14:10:19 +0600

    for loop

--- challenge-2.js
+++ challenge-2.js
@@ -107,3 +107,13 @@ for(let i = 0; i < bills.length; i++) {
 	tips.push(tip);
 	totals.push(tip + bills[i]);
 }
+
+const calcAverage = function (arr) {
+	let sum = 0;
+	for(let i = 0; i < arr.length; i++) {
+		sum+=arr[i];
+	}
+	return sum / arr.length;
+}
+
+console.log(calcAverage(totals));
