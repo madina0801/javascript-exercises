b47ef0ed422de54e5f86d0853a1f3fdd29764176
Madina <madinatussupova0820@gmail.com>
2022-06-01 14:36:58 +0600

    second problem

--- challenge-2.js
+++ challenge-2.js
@@ -126,7 +126,7 @@ console.log(calcAverage(totals));
 const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
 //1. Understanding the problem
-//2. Breaing up into sub-problems
+//2. Breaking up into sub-problems
 
 const calcTempAmplitude = function(temps) {
 	let max = temps[0];
@@ -144,5 +144,24 @@ const calcTempAmplitude = function(temps) {
 	return max-min;
 }
 
-const amplitude = calcTempAmplitude(temperatures);
-console.log(amplitude);
+const calcTempAmplitudeNew = function(t1, t2) {
+ const temps = t1.concat(t2);
+	console.log(temps);
+
+	let max = temps[0];
+	let min = temps[0];
+
+	for(let i = 0; i < temps.length; i++) {
+		const curTemp = temps[i];
+		if(typeof curTemp !== 'number') continue;
+
+
+		if(curTemp > max) max = curTemp;
+		if(curTemp < min) min = curTemp;
+	}
+
+	return max-min;
+}
+
+const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
