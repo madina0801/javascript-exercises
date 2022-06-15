dbcba432d881ebe6970bbfff0f93ba0407e96bf4
Madina <madinatussupova0820@gmail.com>
2022-06-15 15:42:07 +0600

    this keyword practice

--- challenge-2.js
+++ challenge-2.js
@@ -230,6 +230,7 @@ calcAge(1991);
 //HOISTING AND TDZ PRACTICE
 
 // Variables
+/*
 console.log(me);
 // console.log(job);
 // console.log(year);
@@ -271,3 +272,39 @@ const z = 3;
 console.log(x === window.x);
 console.log(y === window.y);
 console.log(z === window.z);
+*/
+
+// THE THIS KEYWORD
+
+// console.log(this);
+
+const calcAge = function(birthYear) {
+	console.log(2037 - birthYear);
+	// console.log(this);
+}
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+	console.log(2037 - birthYear);
+	// console.log(this);
+}
+calcAgeArrow(1991);
+
+const jonas = {
+	year: 1991,
+	calcAge: function() {
+		// console.log(this);
+		console.log(2037 - this.year);
+	}
+}
+
+jonas.calcAge();
+
+const matilda = {
+	year: 2017
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
