bf974ceb3a74970e84181299004d827a881fdcf5
Madina <madinatussupova0820@gmail.com>
2022-06-15 14:24:12 +0600

    var hoisting

--- challenge-2.js
+++ challenge-2.js
@@ -241,8 +241,8 @@ const year = 1991;
 // Functions
 
 console.log(addDecl(2, 3));
-console.log(addExpr(2, 3));
-console.log(addArrow(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
 
 function addDecl(a, b) {
 	return a + b;
@@ -253,3 +253,21 @@ const addExpr = function(a, b) {
 }
 
 var addArrow = (a, b) => a + b;
+
+// Example
+
+if(!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+	console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
