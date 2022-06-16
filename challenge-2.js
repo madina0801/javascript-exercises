abcd6bd1127f2819cf3196cb3153e63f928114ca
Madina <madinatussupova0820@gmail.com>
2022-06-16 10:25:13 +0600

    regular and arrow functions

--- challenge-2.js
+++ challenge-2.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // CHALLENGE 1
 /*
@@ -277,7 +277,7 @@ console.log(z === window.z);
 // THE THIS KEYWORD
 
 // console.log(this);
-
+/*
 const calcAge = function(birthYear) {
 	console.log(2037 - birthYear);
 	// console.log(this);
@@ -308,3 +308,54 @@ matilda.calcAge = jonas.calcAge;
 matilda.calcAge();
 
 const f = jonas.calcAge;
+*/
+
+// REGULAR FUNCTIONS VS ARROW FUNCTIONS
+
+// var firstName = 'Matilda';
+
+const jonas = {
+  firstName: "Jonas",
+  year: 1991,
+  calcAge: function () {
+    console.log(2037 - this.year);
+
+    // SOLUTION 1 - PRE ES6
+    /*
+		const self = this; // self or that
+		const isMillenial = function() {
+			console.log(self);
+			console.log(self.year >= 1981 && self.year <= 1996);
+			// console.log(this.year >= 1981 && this.year <= 1996);
+		};
+		isMillenial();
+*/
+				// SOLUTION 2 - USE ARROW FUNCTIONS
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+
+  greet: () => {
+    console.log(`Hey, ${this.firstName}`);
+  },
+};
+
+jonas.greet();
+jonas.calcAge();
+
+// arguments keyword
+const addExpr = function (a, b) {
+	console.log(arguments);
+	return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+	console.log(arguments);
+	return a + b;
+}
+addArrow(2, 5, 8, 12);
