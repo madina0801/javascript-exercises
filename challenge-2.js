07d061c5e9a9b66b8a1a477e3c1dee66af9c1251
Madina <madinatussupova0820@gmail.com>
2022-07-19 14:14:05 +0600

    map filter reduce practice

--- challenge-2.js
+++ challenge-2.js
@@ -616,5 +616,26 @@ const checkDogs = function(dogsJulia, dogsKate) {
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
 
+const calcAverageHumanAge = function(ages) {
+	const humanAges = ages.map(age => age <= 2 ? 2 * age : 16 + age * 4);
+	// console.log(humanAges);
+	const adultDogs = humanAges.filter(age => age >= 18);
+	// console.log(adultDogs);
+	const avgAge = adultDogs.reduce((acc, age) => acc + age, 0) / adultDogs.length;
+
+	const avgAge1 = adultDogs.reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+	console.log(avgAge1);
+	return avgAge;
+}
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+console.log(avg1);
+console.log(avg2);
+
+// Data 1: [5, 2, 4, 1, 15, 8, 3]
+// Data 2: [16, 6, 10, 5, 6, 1, 4]
+
 // Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
 // Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
